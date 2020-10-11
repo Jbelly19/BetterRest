8c4f5edda76435cf3cb1035de0e2ef8315c2360c
Josh Belmont <jbelmont19@live.com>
2020-10-11 08:14:14 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Josh Belmont on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
